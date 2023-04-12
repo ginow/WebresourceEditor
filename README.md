e58abbbc2c2decced3cc12da7d668c8b5c88969a
Gino <gino.jameskutty@gmail.com>
2023-04-13 00:00:37 +0530

    icon added

--- README.md
+++ README.md
@@ -10,31 +10,12 @@ The solution for above problem is to have an editor like VS Code in the CRM itse
 ## Installation
 
 - Download and import the managed solution `WebresourceEditor_managed.zip` from the `solution` folder or the `Releases` section of this project.
-- Follow below steps to create Custom API definitions:
-
-  1. Create new solution, give any name (eg. CustomAPI)
-  2. In the solution create new Custom API as below:
-
-     Click on `+New` -> `More` -> `Other` -> `Custom API`
-
-     ![steps](resources/steps.png)
-
-     ![Custom API](resources/customapi.png)
-
-  3. Create two Custom API Request Parameters as below:
-
-     ![Content](resources/content.png)
-     ![Webresourceid](resources/webresourceid.png)
-
-  4. Create Custom API Response Parameter as below:
-
-     ![response](resources/response.png)
 
 ## Usage
 
-- Open the HTML file (`<your_org_here>/WebResources/web_editor.html`).
+- Navigate to `Solutions` under settings and click on the `Webresource Editor` button
 
-  For example if your D365 url is `https://orgded007.crm9.dynamics.com/main.aspx` then the URL of the webresource editor app will be `https://orgded007.crm9.dynamics.com/WebResources/web_editor.html`
+  ![solutions](resources/solutions.png)
 
 - Enter any webresource name you want to edit.
 
@@ -49,6 +30,7 @@ The solution for above problem is to have an editor like VS Code in the CRM itse
   ![editor](resources/editor.png)
 
 - Press CTRL+O to open the webresource in new tab.
+- Almost all other keyboard shortcuts of VS Code editor will work as well.
 
 ## References
 
---
+++ solution/unmanagedUnpacked/WebResources/web_editor.js
@@ -0,0 +1,227 @@
+const headers = {
+    "OData-MaxVersion": "4.0",
+    "OData-Version": "4.0",
+    "Accept": "application/json",
+    "Content-Type": "application/json; charset=utf-8",
+    "Prefer": "odata.include-annotations=\"*\""
+}
+const notificationType = {
+    info: 'info',
+    error: 'error',
+    success: 'success'
+}
+const webresourceType = {
+    html: 1,
+    css: 2,
+    javascript: 3
+}
+var notification;
+function showNotification(text, type, duration) {
+    notification.innerText = text;
+    notification.style.display = 'block'
+    notification.className = type
+    duration && setTimeout(hideNotification, duration * 1000);
+}
+function hideNotification() {
+    notification.style.display = 'none'
+}
+async function onLoad() {
+    notification = document.getElementById("notification");
+    createCustomAPI();
+    var language;
+    var webResource = await getWebresource();
+    switch (webResource.webresourcetype) {
+        case webresourceType.html: language = 'html';
+            break;
+        case webresourceType.css: language = 'css';
+            break;
+        case webresourceType.javascript: language = 'javascript';
+            break;
+        default: language = 'text'
+    }
+    // Configure the loader to use the minified version of the editor
+    self.MonacoEnvironment = {
+        getWorkerUrl: function (workerId, label) {
+            return `data:text/javascript;charset=utf-8,${encodeURIComponent(`
+          self.MonacoEnvironment = {
+            baseUrl: 'https://unpkg.com/monaco-editor@0.37.1/min/'
+          };
+          importScripts('https://unpkg.com/monaco-editor@0.37.1/min/vs/base/worker/workerMain.js');`
+            )}`;
+        }
+    };
+    // Load the editor and create an instance
+    require.config({ paths: { 'vs': 'https://unpkg.com/monaco-editor@0.37.1/min/vs' } });
+    require(['vs/editor/editor.main'], function () {
+        var editor = monaco.editor.create(document.getElementById('editorContainer'), {
+            value: webResource.content,
+            language: language,
+            theme: 'vs-dark'
+        });
+        // Define the command to execute when Ctrl+S is pressed (Save the webresource)
+        editor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyS, function () {
+            updateWebresource(editor.getValue(), webResource.webresourceid);
+        })
+        // Define the command to execute when Ctrl+P is pressed (Publish the webresource)
+        editor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyP, function () {
+            publishWebResource(webResource.webresourceid);
+        })
+        // Define the command to execute when Ctrl+W is pressed (Open the webresource)
+        editor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyO, function () {
+            openWebResource(webResource.name);
+        })
+    })
+}
+async function getWebresource() {
+    try {
+        var webResourceName = prompt("Please enter webresource name:", "");
+        document.title = webResourceName;
+        var webResource = {
+            name: webResourceName,
+            webresourceid: null,
+            content: null,
+            webresourcetype: null
+        }
+        showNotification("Retrieving webresource, please wait...", notificationType.info)
+        var response = await fetch(window.location.origin + "/api/data/v9.2/webresourceset?$select=content,webresourceid,webresourcetype&$filter=name eq '" + webResourceName + "'", {
+            method: "GET",
+            headers: headers
+        })
+        const data = await response.json();
+        if (data.value.length > 0) {
+            const encodedContent = data.value[0]["content"];
+            webResource.content = atob(encodedContent);
+            webResource.webresourceid = data.value[0]["webresourceid"];
+            webResource.webresourcetype = data.value[0]["webresourcetype"];
+            showNotification("Webresource retrieved successfully. Use CTRL+S to save, CTRL+P to publish to D365.", notificationType.success, 10);
+            return webResource;
+        }
+        else {
+            showNotification(webResourceName + " not found. Please refresh the page and try again", notificationType.error);
+        }
+    }
+    catch (ex) {
+        showNotification(ex.message, notificationType.error);
+    }
+}
+async function updateWebresource(content, webresourceid) {
+    try {
+        var body = JSON.stringify(
+            {
+                "content": btoa(content),
+                "webresourceid": webresourceid
+            }
+        )
+        showNotification("Saving webresource...", notificationType.info);
+        await fetch(window.location.origin + "/api/data/v9.2/web_update", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: body
+        })
+        showNotification("Webresource saved successfully.", notificationType.success, 5);
+    }
+    catch (ex) {
+        showNotification(ex.message, notificationType.error)
+    }
+}
+async function publishWebResource(webresourceid) {
+    try {
+        var data = {
+            ParameterXml: "<importexportxml><webresources><webresource>{" + webresourceid + "}</webresource></webresources></importexportxml>"
+        };
+        showNotification("Publishing webresource...", notificationType.info);
+        var response = await fetch(window.location.origin + "/api/data/v9.2/PublishXml", {
+            method: "POST",
+            headers: headers,
+            body: JSON.stringify(data)
+        });
+        if (response.ok) {
+            showNotification("Web resource published successfully.", notificationType.success, 5);
+        } else {
+            showNotification(response.statusText, notificationType.error);
+        }
+    } catch (error) {
+        showNotification(error.message, notificationType.error);
+    }
+}
+function openWebResource(name) {
+    const url = window.location.origin + '/WebResources/' + name;
+    window.open(url, "_blank");
+}
+async function createCustomAPI() {
+    try {
+        var response = await fetch(window.location.origin + "/api/data/v9.2/customapis?$filter=uniquename eq 'web_update' &$select=customapiid")
+        const data = await response.json();
+        if (data.value.length == 0) {
+            // API doesn't exist so create it (in default solution)
+            body = {
+                "allowedcustomprocessingsteptype": 0,
+                "boundentitylogicalname": null,
+                "uniquename": "web_update",
+                "displayname": "Update Webresource",
+                "bindingtype": 0,
+                "executeprivilegename": null,
+                "isfunction": false,
+                "isprivate": false,
+                "name": "UpdateWebresource",
+                "description": "UpdateWebresource",
+                "iscustomizable": {
+                    "Value": true
+                },
+                "CustomAPIRequestParameters": [
+                    {
+                        "type": 10,
+                        "isoptional": false,
+                        "displayname": "UpdateWebresource.content",
+                        "name": "UpdateWebresource.content",
+                        "uniquename": "content",
+                        "logicalentityname": null,
+                        "description": "UpdateWebresource.content",
+                        "iscustomizable": {
+                            "Value": true
+                        }
+                    },
+                    {
+                        "type": 10,
+                        "isoptional": false,
+                        "displayname": "UpdateWebresource.webresourceid",
+                        "name": "UpdateWebresource.webresourceid",
+                        "uniquename": "webresourceid",
+                        "logicalentityname": null,
+                        "description": "UpdateWebresource.webresourceid",
+                        "iscustomizable": {
+                            "Value": true
+                        }
+                    }
+                ],
+                "CustomAPIResponseProperties": [
+                    {
+                        "type": 10,
+                        "name": "UpdateWebresource.response",
+                        "logicalentityname": null,
+                        "displayname": "UpdateWebresource.response",
+                        "uniquename": "response",
+                        "description": "UpdateWebresource.response",
+                        "iscustomizable": {
+                            "Value": true
+                        }
+                    }
+                ],
+                "PluginTypeId@odata.bind": "plugintypes(a8de3897-c9de-480c-8007-9e618c9d2ea8)"
+            }
+            await fetch(window.location.origin + "/api/data/v9.2/customapis", {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                    'MSCRM.SolutionUniqueName': 'Default'
+                },
+                body: JSON.stringify(body)
+            })
+        }
+    }
+    catch (error) {
+        showNotification("Error in createCustomAPI: " + error.message);
+    }
+}
