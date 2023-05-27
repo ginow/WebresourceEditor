a64c55935e6aa2821339062da350eeb169918d42
Gino <gino.jameskutty@gmail.com>
2023-05-27 19:38:22 +0530

    working but uses sveltekit

--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -1,16 +1,40 @@
 <script>
 	import { onMount } from 'svelte';
 
-	onMount(async () => {
-		const monaco = await import('monaco-editor');
-		const container = document.getElementById('editorContainer');
-		if (container) {
-			const editor = monaco.editor.create(container, {
-				value: '// Your initial code here',
-				language: 'javascript'
-			});
-		}
-	});
+	if (!import.meta.env.SSR) {
+		// Code that relies on the `window` object
+
+		// Define MonacoEnvironment object
+		window.MonacoEnvironment = {
+			getWorkerUrl: function (_moduleId, label) {
+				if (label === 'json') {
+					return '_dist_/json.worker.js';
+				}
+				if (label === 'css' || label === 'scss' || label === 'less') {
+					return '_dist_/css.worker.js';
+				}
+				if (label === 'html' || label === 'handlebars' || label === 'razor') {
+					return '_dist_/html.worker.js';
+				}
+				if (label === 'typescript' || label === 'javascript') {
+					return '_dist_/ts.worker.js';
+				}
+				return '_dist_/editor.worker.js';
+			}
+		};
+
+		onMount(async () => {
+			const monaco = await import('monaco-editor');
+			const container = document.getElementById('editorContainer');
+			if (container) {
+				const editor = monaco.editor.create(container, {
+					value: '// Your initial code here',
+					language: 'javascript',
+					theme: 'vs-dark'
+				});
+			}
+		});
+	}
 </script>
 
 <div id="editorContainer" style="width: 600px; height: 400px;" />
--- svelte/vite.config.js
+++ svelte/vite.config.js
@@ -1,6 +1,21 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-
+import workerPlugin from 'worker-plugin';
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit(), workerPlugin()],
+	build: {
+		outDir: "dist",
+		assetsDir: "",
+		sourcemap: true, // generate source maps
+		cssCodeSplit: false, // generate a single CSS file
+		target: "esnext", // disable code splitting
+		rollupOptions: {
+			output: {
+				entryFileNames: "WebResources/web_editorv2.js", // Rename the JS file
+				// chunkFileNames: "sveltepowerapps.js", // Rename the chunks
+				assetFileNames: "WebResources/web_editorv2.css", // Rename the CSS file
+			},
+		},
+		// chunkSizeWarningLimit: 7000, // disable chunk splitting
+	}
 });
