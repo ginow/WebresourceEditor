416a16caedf3ec887c5d83dcbea97d6644ca87f9
Gino <gino.jameskutty@gmail.com>
2023-05-28 13:13:40 +0530

    tab close css fixed

--- svelte/src/lib/Tabs.svelte
+++ svelte/src/lib/Tabs.svelte
@@ -3,7 +3,7 @@
 
   let activeTabIndex = 0;
   let tabs = [
-    { id: 1, title: "File 1" },
+    { id: 1, title: "some long name of the File 1" },
     { id: 2, title: "File 2" },
     { id: 3, title: "File 3" },
     { id: 4, title: "File 4" },
@@ -21,9 +21,13 @@
   }
 
   function closeTab(index) {
+    const wasActiveTab = index === activeTabIndex;
+
     tabs.splice(index, 1);
-    if (activeTabIndex >= tabs.length) {
-      activeTabIndex = tabs.length - 1;
+    if (wasActiveTab && index > 0) {
+      activeTabIndex = index - 1;
+    } else if (wasActiveTab && index === 0 && tabs.length > 0) {
+      activeTabIndex = 1;
     }
   }
 
@@ -42,27 +46,31 @@
       <div
         class="tab {index === activeTabIndex && 'active'}"
         on:click={() => switchTab(index)}
+        on:keydown={() => {
+          /*TODO*/
+        }}
       >
         {tab.title}
-        <span
-          class="close-btn"
-          tabindex="0"
-          on:click={() => closeTab(index)}
-          on:keydown={(event) => {
-            if (event.key === "Enter" || event.key === " ") {
-              closeTab(index);
-            }
-          }}
-          ><svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 50 50"
-            width="50px"
-            height="50px"
-            ><path
-              d="M 7.71875 6.28125 L 6.28125 7.71875 L 23.5625 25 L 6.28125 42.28125 L 7.71875 43.71875 L 25 26.4375 L 42.28125 43.71875 L 43.71875 42.28125 L 26.4375 25 L 43.71875 7.71875 L 42.28125 6.28125 L 25 23.5625 Z"
-            /></svg
-          >
-        </span>
+        {#if index === activeTabIndex}
+          <span
+            class="close-btn"
+            on:click={() => closeTab(index)}
+            on:keydown={(event) => {
+              if (event.key === "Enter" || event.key === " ") {
+                closeTab(index);
+              }
+            }}
+            ><svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 50 50"
+              width="50px"
+              height="50px"
+              ><path
+                d="M 7.71875 6.28125 L 6.28125 7.71875 L 23.5625 25 L 6.28125 42.28125 L 7.71875 43.71875 L 25 26.4375 L 42.28125 43.71875 L 43.71875 42.28125 L 26.4375 25 L 43.71875 7.71875 L 42.28125 6.28125 L 25 23.5625 Z"
+              /></svg
+            >
+          </span>
+        {/if}
       </div>
     {/each}
   </div>
@@ -79,26 +87,27 @@
     display: flex;
     overflow-x: auto;
     background-color: #333;
-    padding: 0; /* Remove padding */
-    margin-bottom: -2px; /* Remove negative margin to remove space between tabs */
-    scrollbar-width: thin; /* Add thin scrollbar */
-    scrollbar-color: #ccc #f5f5f5; /* Set scrollbar colors */
+    padding: 0;
+    margin-bottom: -2px;
+    scrollbar-width: thin;
+    scrollbar-color: #ccc #f5f5f5;
   }
 
   .tab {
     display: flex;
     align-items: center;
-    padding: 8px 12px;
+    padding: 8px 30px 8px 12px;
     background-color: #444;
     cursor: pointer;
-    color: #fff;
+    color: #a4a4a4;
     transition: background-color 0.3s ease;
+    position: relative; /* Add relative positioning */
   }
 
   .tab.active {
     background-color: #212121;
     border-bottom: 1px solid #ccc;
-    color: #6f6f6f;
+    color: #a4a4a4;
   }
 
   .content {
@@ -106,11 +115,15 @@
     border: 1px solid #ccc;
     border-top: none;
     background-color: #333;
-    color: #fff;
+    color: #a4a4a4;
   }
 
   .close-btn {
-    margin-left: 5px;
+    display: none; /* Hide the close button by default */
+    position: absolute; /* Position the close button */
+    top: 50%;
+    right: 8px;
+    transform: translateY(-50%);
     cursor: pointer;
   }
 
@@ -120,14 +133,18 @@
     fill: #fff;
   }
 
+  .tab:hover .close-btn {
+    display: inline-block; /* Show the close button on hover */
+  }
+
   /* Customize scroll bar */
   ::-webkit-scrollbar {
-    height: 2px; /* Set the height of the scrollbar */
+    height: 2px;
     width: 20px;
-    background-color: #f5f5f5; /* Light grey background color */
+    background-color: #f5f5f5;
   }
 
   ::-webkit-scrollbar-thumb {
-    background-color: #ccc; /* Light grey thumb color */
+    background-color: #ccc;
   }
 </style>
