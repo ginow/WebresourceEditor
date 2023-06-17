7cf15977092409b5129f83a656cb694439b2cab9
Gino <gino.jameskutty@gmail.com>
2023-06-17 07:47:30 +0530

    CloseIcon moved to seperate svelte file

--- svelte/src/lib/Tabs.svelte
+++ svelte/src/lib/Tabs.svelte
@@ -1,7 +1,7 @@
 <script>
   import { onMount } from "svelte";
   import { writable } from "svelte/store";
-
+  import CloseIcon from "../assets/CloseIcon.svelte";
   let activeTabIndex = 0;
   export let tabs;
 
@@ -69,11 +69,7 @@
               }
             }}
           >
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50">
-              <path
-                d="M 7.71875 6.28125 L 6.28125 7.71875 L 23.5625 25 L 6.28125 42.28125 L 7.71875 43.71875 L 25 26.4375 L 42.28125 43.71875 L 43.71875 42.28125 L 26.4375 25 L 43.71875 7.71875 L 42.28125 6.28125 L 25 23.5625 Z"
-              />
-            </svg>
+            <CloseIcon />
           </span>
         {/if}
       </div>
